78c98c09c76fa8f6e40f1dbe101db9c51579d10d
dottox <imsm2424@gmail.com>
2025-06-23 23:38:34 -0300

    chore: added seller to product model and enchanced the class

--- src/main/java/com/ecommerce/project/models/Product.java
+++ src/main/java/com/ecommerce/project/models/Product.java
@@ -1,20 +1,31 @@
 package com.ecommerce.project.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
+@ToString
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name must be at least 3 characters long")
     private String name;
     private String image;
+
+    @NotBlank
+    @Size(min = 10, max = 500, message = "Description must be between 10 and 500 characters")
     private String description;
     private Integer quantity;
     private Double price;
@@ -24,4 +35,8 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
