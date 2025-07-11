03ad42cf02a9a95848c4b967d15811e0d4ea893b
dottox <imsm2424@gmail.com>
2025-07-11 00:34:55 -0300

    feat; added CartItemRepository

---
+++ src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -0,0 +1,14 @@
+package com.ecommerce.project.repositories;
+
+import com.ecommerce.project.models.Cart;
+import com.ecommerce.project.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    Optional<CartItem> findCartItemByCartIdAndProductId(Long cartId, Long productId);
+    // Additional methods for CartItem can be defined here if needed
+}
