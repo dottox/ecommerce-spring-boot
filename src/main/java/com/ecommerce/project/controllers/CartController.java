5020a55f229b02c03689e17ad89d47c1e15f92cf
dottox <imsm2424@gmail.com>
2025-07-16 00:42:00 -0300

    feat: endpoint GET allCarts in /carts

--- src/main/java/com/ecommerce/project/controllers/CartController.java
+++ src/main/java/com/ecommerce/project/controllers/CartController.java
@@ -9,15 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class CartController {
 
     @Autowired
     private CartService cartService;
-
-    @Autowired
-    private CartRepository cartRepository;
 
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
     public ResponseEntity<CartDTO> addProductToCart(
@@ -27,5 +26,11 @@ public class CartController {
         // Logic to add product to cart
         CartDTO savedCartDTO = cartService.addProductToCart(productId, quantity);
         return new ResponseEntity<CartDTO>(savedCartDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/carts")
+    public ResponseEntity<List<CartDTO>> getAllCarts() {
+        List<CartDTO> cartDTOs = cartService.getAllCarts();
+        return new ResponseEntity<>(cartDTOs, HttpStatus.OK);
     }
 }
