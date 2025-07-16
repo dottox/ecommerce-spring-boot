c4374e9340949ab8f30b5bb197e5ebdc3d56f6f3
dottox <imsm2424@gmail.com>
2025-07-16 00:44:49 -0300

    fix: Optional unnecesary in findCartByEmail in CartRepository

--- src/main/java/com/ecommerce/project/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
-    Optional<Cart> findCartByEmail(String email);
+    Cart findCartByEmail(String email);
 }
