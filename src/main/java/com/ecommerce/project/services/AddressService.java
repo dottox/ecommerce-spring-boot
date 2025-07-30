e57630681b0385310edef5494808c28a48b34481
dottox <imsm2424@gmail.com>
2025-07-30 19:44:52 -0300

    feat: services functions for all these endpoints -> 686f75b21f4a37435fa3aaf7bc64a0e3101efdc4

--- src/main/java/com/ecommerce/project/services/AddressService.java
+++ src/main/java/com/ecommerce/project/services/AddressService.java
@@ -2,7 +2,15 @@ package com.ecommerce.project.services;
 
 import com.ecommerce.project.models.User;
 import com.ecommerce.project.payload.AddressDTO;
+import jakarta.validation.Valid;
+
+import java.util.List;
 
 public interface AddressService {
-    AddressDTO createAddress(AddressDTO addressDTO, User user);
+    AddressDTO createAddress(AddressDTO addressDTO);
+    List<AddressDTO> getAllAddresses();
+    AddressDTO getAddressById(Long addressId);
+    List<AddressDTO> getLoggedUserAddresses();
+    AddressDTO updateAddress(Long addressId, AddressDTO addressDTO);
+    AddressDTO deleteAddress(Long addressId);
 }
