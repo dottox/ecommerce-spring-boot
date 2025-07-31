d93c9ca5e22f2fcb5170f19350f867a5c2a813ee
dottox <imsm2424@gmail.com>
2025-07-31 19:34:47 -0300

    docs: added tag an operation to CartController's endpoints

--- src/main/java/com/ecommerce/project/controllers/CartController.java
+++ src/main/java/com/ecommerce/project/controllers/CartController.java
@@ -4,6 +4,8 @@ import com.ecommerce.project.payload.CartDTO;
 import com.ecommerce.project.repositories.CartRepository;
 import com.ecommerce.project.services.CartService;
 import com.ecommerce.project.util.AuthUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,8 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @Tag(name = "Cart Management", description = "APIs for managing shopping carts")
+    @Operation(summary = "Add product to cart", description = "API to add a product to the logged user's cart with specified quantity")
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
     public ResponseEntity<CartDTO> addProductToCart(
             @PathVariable Long productId,
@@ -30,18 +34,24 @@ public class CartController {
         return new ResponseEntity<CartDTO>(savedCartDTO, HttpStatus.CREATED);
     }
 
+    @Tag(name = "Cart Management", description = "APIs for managing shopping carts")
+    @Operation(summary = "Get all carts", description = "API to retrieve all carts")
     @GetMapping("/carts")
     public ResponseEntity<List<CartDTO>> getAllCarts() {
         List<CartDTO> cartDTOs = cartService.getAllCarts();
         return new ResponseEntity<>(cartDTOs, HttpStatus.OK);
     }
 
+    @Tag(name = "Cart Management", description = "APIs for managing shopping carts")
+    @Operation(summary = "Get cart", description = "API to retrieve the cart of the logged-in user")
     @GetMapping("/carts/users/cart")
     public ResponseEntity<CartDTO> getCartLoggedUser() {
         CartDTO cartDTO = cartService.getCartLoggedUser();
         return new ResponseEntity<>(cartDTO, HttpStatus.OK);
     }
 
+    @Tag(name = "Cart Management", description = "APIs for managing shopping carts")
+    @Operation(summary = "Update product quantity in cart", description = "API to update the quantity of a product in the logged user's cart")
     @PutMapping("/carts/products/{productId}/quantity/{operation}")
     public ResponseEntity<CartDTO> updateProductQuantity(
             @PathVariable Long productId,
@@ -52,6 +62,8 @@ public class CartController {
         return new ResponseEntity<>(updatedCartDTO, HttpStatus.OK);
     }
 
+    @Tag(name = "Cart Management", description = "APIs for managing shopping carts")
+    @Operation(summary = "Delete product from cart", description = "API to delete a product from the logged user's cart")
     @DeleteMapping("/carts/products/{productId}")
     public ResponseEntity<CartDTO> deleteProductFromCart(
             @PathVariable Long productId
